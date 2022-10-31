acd3bc45156b76bed1e18f6c5915606426947d2c
Rajesh-Dukka <rajeshnaidu886@gmail.com>
2022-10-31 18:30:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PhotoGallery from './components/photoGallery'
+
+const App = () => {
+  return (
+    <div>
+      <PhotoGallery />
+    </div>
+  )
+}
+
+export default App
