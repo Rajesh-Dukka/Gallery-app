d33d949db215464f31a6e148e3dee2281b0e3d8f
Rajesh-Dukka <rajeshnaidu886@gmail.com>
2022-11-15 13:01:40 +0530

    test1

--- src/components/photoGallery.js
+++ src/components/photoGallery.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Images.css";
 
 const PhotoGallery = () => {
   const [imagesList, setImagesList] = useState([]);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     const task = async () => {
@@ -15,6 +18,11 @@ const PhotoGallery = () => {
     };
     task()
   }, []);
+
+  const showImageModal=(image,i)=>{
+    console.log(image,i)
+    setShow({image})
+    }
 let myCurrentDate = new Date()
 let date = myCurrentDate.getDate();
 let month = myCurrentDate.getMonth() + 1;
@@ -22,19 +30,39 @@ let year = myCurrentDate.getFullYear();
 
   return (
     <div className="app-container">
+      {show.image&&
+    <>
+  <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+           </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={show.image} alt="" className="modalImage"/>
+        </Modal.Body>
+      </Modal>
+  </>
+    }
       
       <div className="images-container">
         {imagesList.map((each, i) => {
+          const img=`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`
           return (
             <div className="image-container" key={i}>
               <img
-                src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
+                src={img}
                 className="image"
+                onClick={()=>showImageModal(img,i)}
                 alt="shopping-images"
               />
               <div >
               <p className="title">{each.title} </p>
-              <p className="date">{date}/{month<10?`0${month}`:`${month}`}/{year}</p>
+              <p className="date">{date-i}/{month<10?`0${month}`:`${month}`}/{year}</p>
               </div>
             </div>
           );
